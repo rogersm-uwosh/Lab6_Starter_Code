bbb2a5c8a47b9f34410d35fc1f6b2fec60efa620
aljohnston112 <aljohnston112@gmail.com>
2024-10-15 20:38:54 -0500

    ConvertBack is not implemented

--- NearbyAirports/AirportVisitedConvertor.cs
+++ NearbyAirports/AirportVisitedConvertor.cs
@@ -23,7 +23,7 @@ public class AirportVisitedConvertor : IValueConverter
 
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
     {
-        throw new NotImplementedException();
+        return null;
     }
     
 }
