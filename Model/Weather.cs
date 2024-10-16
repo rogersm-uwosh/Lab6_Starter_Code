9e5efe64d48b1e306ed862415a15e20927c61b3f
Peter Skogman <skogmanpe40@uwosh.edu>
2024-10-16 17:08:08 -0500

    weather page group 3

---
+++ Model/Weather.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace Lab6_Starter.Model;
+
+[Serializable()]
+public class Weather : INotifyPropertyChanged
+{
+    String metar;
+    String taf;
+
+    public String Metar 
+    {
+        get { return metar; }
+        set
+        {
+            metar = value;
+            OnPropertyChanged(nameof(Metar));
+        }
+    }
+
+    public String Taf
+    {
+        get { return taf; }
+        set
+        {
+            taf = value;
+            OnPropertyChanged(nameof(Taf));
+        }
+    }
+
+    public Weather(String metar, String taf)
+    {
+        Metar = metar;
+        Taf = taf;
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected virtual void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
