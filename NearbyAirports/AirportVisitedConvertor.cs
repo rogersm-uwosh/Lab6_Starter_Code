9a6994631a0100a114ee9aa48577efc3e13dab37
aljohnston112 <aljohnston112@gmail.com>
2024-10-15 20:37:40 -0500

    Team 2 Added the nearby airports page prototype.

---
+++ NearbyAirports/AirportVisitedConvertor.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+
+namespace Lab2_Solution.NearbyAirports;
+
+/// <summary>
+/// Alexander Johnston wrote this class
+///
+/// Converts boolean to a red circle or green circle image source.
+/// </summary>
+public class AirportVisitedConvertor : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is bool isActive)
+        {
+            return isActive
+                ? "green_circle.png"
+                : "red_circle.png";
+        }
+
+        return null;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+    
+}
---
+++ NearbyAirports/NearbyAirportsPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.ObjectModel;
+using Lab2_Solution.Model;
+
+namespace Lab2_Solution.NearbyAirports;
+
+/// <summary>
+/// Written by Hiba Seraj
+/// Alexander Johnston forked and cloned the project, and added the navigation.
+/// A page that displays nearby airports.
+/// </summary>
+public partial class NearbyAirportsPage : ContentPage
+{
+    public ObservableCollection<NearbyAirport> NearbyAirports { get; } = [];
+
+    public NearbyAirportsPage()
+    {
+        InitializeComponent();
+        BindingContext = this;
+        NearbyAirports.Add(new NearbyAirport("KFLD", "Fond du Lac", 10, true));
+        NearbyAirports.Add(new NearbyAirport("KMTW", "Manitowac", 15,true));
+        NearbyAirports.Add(new NearbyAirport("79C", "Brenner", 18,false));
+        NearbyAirports.Add(new NearbyAirport("KUNU", "Dodge County", 64,true));
+    }
+    
+}
+
+
