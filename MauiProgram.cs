40bc1152fdf3fc9d8ef6fc0072da3fd85ca0163c
Caden Rohan <rohanc94@uwosh.edu>
2024-10-18 16:40:39 -0500

    Reformatted xaml

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -13,7 +13,7 @@ public static class MauiProgram
     public static MauiApp CreateMauiApp()
     {
 		var builder = MauiApp.CreateBuilder();
-		builder.ConfigureSyncfusionCore();
+		builder.ConfigureSyncfusionCore(); // for selecting date with calendar
 		builder
 			.UseMauiApp<App>()
             .UseMauiCommunityToolkit()
