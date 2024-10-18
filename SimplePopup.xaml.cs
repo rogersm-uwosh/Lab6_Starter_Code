535624f73edd7f2d80bd36b495b8f141d4587dc1
Jake Robbins <robbij25@uwosh.edu>
2024-10-18 14:20:48 -0500

    Added a row of stars to "rating" field that start greyed out, then turn yellow when clicked to indicate the rating. They do not connect to any sort of model yet.

--- SimplePopup.xaml.cs
+++ SimplePopup.xaml.cs
@@ -1,9 +1,13 @@
 using CommunityToolkit.Maui.Views;
+using System.Formats.Tar;
 namespace Lab6_Starter;
 
 public partial class SimplePopup : CommunityToolkit.Maui.Views.Popup
 {
-	public SimplePopup()
+
+    const string greyStarPath = "Resources/Images/ic_fluent_star_24_filled_grey.svg";
+    const string yellowStarPath = "Resources/Images/ic_fluent_star_24_filled_yellow.svg";
+    public SimplePopup()
 	{
 		InitializeComponent();
 	}
@@ -11,5 +15,45 @@ public partial class SimplePopup : CommunityToolkit.Maui.Views.Popup
 	void CancelAirportAdd_Clicked(System.Object sender, System.EventArgs e) {
 		Close();
 	}
+    private void StarOne_Clicked(object sender, EventArgs e)
+    {
+        starOne.Source = yellowStarPath;
+        starTwo.Source = greyStarPath;
+        starThree.Source = greyStarPath;
+        starFour.Source = greyStarPath;
+        starFive.Source = greyStarPath;
+    }
+    private void StarTwo_Clicked(object sender, EventArgs e)
+    {
+        starOne.Source = yellowStarPath;
+        starTwo.Source = yellowStarPath;
+        starThree.Source = greyStarPath;
+        starFour.Source = greyStarPath;
+        starFive.Source = greyStarPath;
+    }
+    private void StarThree_Clicked(object sender, EventArgs e)
+    {
+        starOne.Source = yellowStarPath;
+        starTwo.Source = yellowStarPath;
+        starThree.Source = yellowStarPath;
+        starFour.Source = greyStarPath;
+        starFive.Source = greyStarPath;
+    }
+    private void StarFour_Clicked(object sender, EventArgs e)
+    {
+        starOne.Source = yellowStarPath;
+        starTwo.Source = yellowStarPath;
+        starThree.Source = yellowStarPath;
+        starFour.Source = yellowStarPath;
+        starFive.Source = greyStarPath;
+    }
+    private void StarFive_Clicked(object sender, EventArgs e)
+    {
+        starOne.Source = yellowStarPath;
+        starTwo.Source = yellowStarPath;
+        starThree.Source = yellowStarPath;
+        starFour.Source = yellowStarPath;
+        starFive.Source = yellowStarPath;
+    }
 
 }
