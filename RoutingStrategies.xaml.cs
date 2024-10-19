8f78373eb39fb7215da9ae99e2c4192edcbe823a
Hannah <hannahhotchkiss08@gmail.com>
2024-10-19 14:04:51 -0500

    Switched the ListView to a CollectionView, left-aligned the suggested routes table, and lined up the entries

--- RoutingStrategies.xaml.cs
+++ RoutingStrategies.xaml.cs
@@ -3,6 +3,7 @@ using Microsoft.Maui.Controls.Maps;
 using Microsoft.Maui.Maps;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using Map = Microsoft.Maui.Controls.Maps.Map;
 
 
@@ -10,19 +11,23 @@ namespace Lab6_Starter
 {
     public partial class RoutingStrategies : TabbedPage
     {
+         public ObservableCollection<Route> Routes { get; set; }
+
         public RoutingStrategies()
         {
             InitializeComponent();
 
             // Fill data for ListView (Hannah)
-            RoutesListView.ItemsSource = new List<Route>
+            // Example data to bind
+            Routes = new ObservableCollection<Route>
             {
-                new Route { ICAO = "KATW", City = "Appleton", Distance = 0 },
-                new Route { ICAO = "KFLD", City = "Fond du Lac", Distance = 29 },
-                new Route { ICAO = "KUNU", City = "Dodge County", Distance = 23 },
-                new Route { ICAO = "KBUU", City = "Burlington", Distance = 47 },
-                new Route { ICAO = "KATW", City = "Appleton", Distance = 95 }
+                new Route { ICAO = "KATL", City = "Atlanta", Distance = 100 },
+                new Route { ICAO = "KLAX", City = "Los Angeles", Distance = 200 },
+                new Route { ICAO = "KJFK", City = "New York", Distance = 300 }
             };
+
+            // Binding the CollectionView to the data source
+            RoutesCollectionView.ItemsSource = Routes;
 
             // Set initial map location (Pachia)
             map.MoveToRegion(MapSpan.FromCenterAndRadius(new Location(44.2619, 88.4154), Distance.FromMiles(10)));
