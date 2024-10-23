bb49c77842b3d757d7a4eae0642234f4346a05af
andrewmartinx <martia42@uwosh.edu>
2024-10-23 13:29:42 -0500

    Added button navigation, changed button radius and font size, removed button screens from navigation bar

--- PlanningTools.xaml.cs
+++ PlanningTools.xaml.cs
@@ -1,3 +1,5 @@
+using Lab2_Solution.NearbyAirports;
+
 namespace Lab6_Starter;
 
 public partial class PlanningTools : ContentPage
@@ -6,4 +8,27 @@ public partial class PlanningTools : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void OnNearbyAirportsClicked(object sender, EventArgs e)
+    {
+        // Navigate to the Nearby Airports page
+        await Navigation.PushAsync(new NearbyAirportsPage());
+    }
+    private async void OnRoutingStrategiesClicked(object sender, EventArgs e)
+    {
+        // Navigate to the Routing Strategies page
+        await Navigation.PushAsync(new RoutingStrategies());
+    }
+
+    private async void OnWeatherClicked(object sender, EventArgs e)
+    {
+        // Navigate to the Weather page
+        await Navigation.PushAsync(new WeatherPage());
+    }
+
+    private async void OnTShirtClicked(object sender, EventArgs e)
+    {
+        // Navigate to the TShirts page
+        //await Navigation.PushAsync(new WeatherPage());
+    }
 }
