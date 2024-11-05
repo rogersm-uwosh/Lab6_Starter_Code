92193d16faffd65d6536fefb250bf0b04426f871
jbender3 <benderje26@uwosh.edu>
2024-11-05 12:21:01 -0600

    Switched WebView to use api for OpenStreetMap
    - Now it directly uses the Mapsui API, which will allow us to draw lines in the near future
    - It is focused on Wisconsin
    - It is now a little taller, so now the RoutingStrategies page is in a scrollview

--- RoutingStrategies.xaml.cs
+++ RoutingStrategies.xaml.cs
@@ -1,11 +1,7 @@
-using Mapsui.UI.Maui;
-using Mapsui.Layers;
+using Mapsui;
 using Mapsui.Tiling;
-using Mapsui.Utilities;
-using Microsoft.Maui.Controls;
-using System.Collections.ObjectModel;
-using Mapsui.Projections;
 using Lab6_Starter.Model;
+using Mapsui.Projections;
 
 namespace Lab6_Starter
 {
@@ -25,14 +21,25 @@ namespace Lab6_Starter
             // Set the BindingContext for data binding
             BindingContext = this;
 
-            // Load OpenStreetMap directly into the WebView
-            var mapUrl = GenerateOpenStreetMapUrl(44.2619, -88.4154, 10); // Appleton Airport coordinates
-            MapView.Source = mapUrl;
+            Mapsui.Map map = new();
+
+            // Initialize the map with OpenStreetMap's API and add it to MapPage's content
+            map.Layers.Add(OpenStreetMap.CreateTileLayer());
+            map.Home = ZoomToWisconsin;
+
+            RouteMap.Map = map;
         }
 
-        private string GenerateOpenStreetMapUrl(double latitude, double longitude, int zoom)
+        private void ZoomToWisconsin(Navigator n) {
+            ZoomMapTo(n, 41.7, -93.1, 47.3, -86); // Wisconsin coordinates
+        }
+
+        private static void ZoomMapTo(Navigator n, double latitudeMin, double longitudeMin, double latitudeMax, double longitudeMax)
         {
-            return $"https://www.openstreetmap.org/?mlat={latitude}&mlon={longitude}#map={zoom}/{latitude}/{longitude}";
+            (double minX, double minY) = SphericalMercator.FromLonLat(longitudeMin, latitudeMin);
+            (double maxX, double maxY) = SphericalMercator.FromLonLat(longitudeMax, latitudeMax);
+
+            n.ZoomToBox(new MRect(minX, minY, maxX, maxY), MBoxFit.Fit);
         }
     }
 }
