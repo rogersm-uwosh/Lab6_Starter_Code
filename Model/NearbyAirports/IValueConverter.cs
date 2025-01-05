8953e1896045d33b2b56e99d7621db1439bd1722
mprogers <mprogers@users.noreply.github.com>
2025-01-05 09:25:32 -0600

    Got NearbyAirports working (again)
    There was a bug that broke NearbyAirports. AirportToMilesConverter was
    supposed to implement IValueConverter, which is part of .NET MAUI. But
    the precursor to this port was console based, so I defined my own version
    of IValueConverter so that it would compile. When that was deleted in this
    app, the .NET MAUI version was used instead, and now NearbyAirports displays
    mileage.
    
    Also, there was a typo, Convertor => Converter

--- Model/NearbyAirports/IValueConverter.cs
+++
@@ -1,9 +0,0 @@
-using System.Globalization;
-
-namespace Lab6_Starter.Model.NearbyAirports;
-
-public interface IValueConverter
-{
-    object Convert(object value, Type targetType, object parameter, CultureInfo culture);
-    object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture);
-}
