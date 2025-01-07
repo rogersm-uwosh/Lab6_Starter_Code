4826b53fe7b82927e10269e8c199f1a80c760ae4
aljohnston112 <aljohnston112@gmail.com>
2025-01-07 10:09:24 -0600

    Readded the GetRoute fix and fixed the comment on the method.

--- Model/BusinessLogic.cs
+++ Model/BusinessLogic.cs
@@ -238,41 +238,43 @@ public partial class BusinessLogic : IBusinessLogic
     {
         return db.GetAllWisconsinAirports();
     }
-
-     /// <summary>
-     /// Get the weather of the closest airport
-     /// </summary>
-     /// <returns>The weather of the closest airport</returns>
     
-    public Route GetRoute(WisconsinAirport source, int maxMiles, bool unvisitedOnly)
+    /// <summary>
+    /// Finds a route to visit all airports within a specific range from a starting airport.
+    /// The route will end at the starting airport.
+    /// </summary>
+    /// <param name="source">The starting airport.</param>
+    /// <param name="maxMiles">How many miles from the source the airports in the route should be.</param>
+    /// <param name="unvisitedOnly">If true, then visited airports will not be in the route.</param>
+    /// <returns></returns>
+    public Route? GetRoute(WisconsinAirport source, int maxMiles, bool unvisitedOnly)
     {
-        // // We need to force the start to be at the beginning, so we remove it
-        // // and possibly already visited airports
-        // IEnumerable<Airport> excluded;
-        // if (unvisitedOnly)
-        // {
-        //     excluded = GetAirports().Append(source);
-        // }
-        // else
-        // {
-        //     excluded = [source];
-        // }
-        // // Convert the airports to RoutePoints
-        // List<RoutePoint> routePoints = CalculateNearbyAirports(source, maxMiles)
-        //     .Except(excluded, new AirportEqualityComparer())
-        //     .Prepend(source)
-        //     .Select(x => new RoutePoint(x))
-        //     .ToList();
+        // We need to force the start to be at the beginning, so we remove it
+        // and possibly already visited airports
+        List<WisconsinAirport> excluded = [source];
+        if (unvisitedOnly)
+        {
+            IEnumerable<WisconsinAirport> allAirports = GetAllWisconsinAirports();
+            IEnumerable<string?> visitedAirportIds = GetVisitedAirports().Result.Select(airport => airport.Id);
+            excluded.AddRange(
+                allAirports.Where(airport => !visitedAirportIds.Contains(airport.Id))
+            );
+        }
 
-        // // Can't have a route with 0 or 1 airports
-        // if (routePoints.Count < 2)
-        // {
-        //     return null;
-        // }
+        // Convert the airports to RoutePoints
+        List<RoutePoint> routePoints = CalculateNearbyAirports(source, maxMiles)
+            .Except(excluded, new WisconsinAirportEqualityComparer())
+            .Prepend(source)
+            .Select(x => new RoutePoint(x))
+            .ToList();
 
-        // return Route.GenerateTravelingSalesmanRoute(routePoints);
+        // Can't have a route with 0 or 1 airports
+        if (routePoints.Count < 2)
+        {
+            return null;
+        }
 
-        return null;
+        return Route.GenerateTravelingSalesmanRoute(routePoints);
     }
 
 }
