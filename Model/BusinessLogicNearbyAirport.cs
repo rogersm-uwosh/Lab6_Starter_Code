a446f76fc3353e4e6acaa2d9f4c37163da014cee
aljohnston112 <aljohnston112@gmail.com>
2025-01-27 08:57:10 -0600

    Correctly merged the nearby airport's business logic.

--- Model/BusinessLogicNearbyAirport.cs
+++ Model/BusinessLogicNearbyAirport.cs
@@ -36,28 +36,61 @@ public partial class BusinessLogic
     /// <returns></returns>
     public void CalculateNearbyAirports(WisconsinAirport sourceAirport, int maxMiles)
     {
-        NearbyAirports.Clear();
         Dictionary<string, int> idToMiles = new();
+        List<WisconsinAirport> tempNearbyAirports = new();
 
         ObservableCollection<WisconsinAirport> allAirports = GetWisconsinAirports();
 
-        foreach (WisconsinAirport destinationAirport in allAirports)
+        foreach (WisconsinAirport destinationAirport in allAirports) // why aren't we using a dictionary here??
         {
-            double distanceInMiles = GetDistanceBetweenCoordinates(
-                sourceAirport.Coordinates,
-                destinationAirport.Coordinates
+            WisconsinAirport? destinationAirportCoordinates = WisconsinAirports.FirstOrDefault(
+                airport => airport?.Id == destinationAirport.Id && airport.Id != sourceAirport.Id, null
             );
-            if (distanceInMiles < maxMiles)
+
+            if (destinationAirportCoordinates != null)
             {
-                NearbyAirports.Add(destinationAirport);
-                idToMiles[destinationAirport.Id] = (int)Math.Round(distanceInMiles);
+                double distanceInMiles = GetDistanceFromAirportCoordinates(
+                    sourceAirport,
+                    destinationAirportCoordinates
+                );
+                if (distanceInMiles < maxMiles)
+                {
+                    tempNearbyAirports.Add(destinationAirport);
+                    idToMiles[destinationAirport.Id] = (int)Math.Round(distanceInMiles);
+                }
             }
         }
 
-        IsNearbyAirportsHeaderVisible = NearbyAirports.Count > 0;
+        AirportToMilesConverter.ConvertAll(idToMiles); // converter now has distances to display
 
-        // ConvertAll is used to populate _idToMiles with the (id, distance) entries
-        // This allows the AirportToMilesConverter to correctly display distance in XAML
-        AirportToMilesConverter.ConvertAll(idToMiles); 
+        NearbyAirports.Clear();
+        foreach (var airport in tempNearbyAirports)
+        {
+            NearbyAirports.Add(airport);
+        }
+
+        IsNearbyAirportsHeaderVisible = NearbyAirports.Count > 0; // no airports? no headers necessary
+    }
+
+    public static double GetDistanceFromAirportCoordinates(
+        WisconsinAirport sourceAirport,
+        WisconsinAirport destinationAirport
+    )
+    {
+        // Haversine formula to find distance between two points
+        double sourceLatitudeRadians = sourceAirport.Latitude * (Math.PI / 180);
+        double destinationLatitudeRadians = destinationAirport.Latitude * (Math.PI / 180);
+        double latitudeDiffRadians =
+            (destinationAirport.Latitude - sourceAirport.Latitude) * (Math.PI / 180);
+        double longitudeDiffRadians =
+            (destinationAirport.Longitude - sourceAirport.Longitude) * (Math.PI / 180);
+        double flatDistance = Math.Pow(Math.Sin(latitudeDiffRadians / 2.0), 2.0) +
+                              (Math.Cos(sourceLatitudeRadians) *
+                               Math.Cos(destinationLatitudeRadians) *
+                               Math.Pow(Math.Sin(longitudeDiffRadians / 2.0), 2.0));
+        double angularDistance = 2 * Math.Atan2(Math.Sqrt(flatDistance), Math.Sqrt(1 - flatDistance));
+        double distanceInMeters = EARTH_RADIUS_IN_METERS * angularDistance;
+        double distanceInMiles = distanceInMeters * MILES_PER_METER;
+        return distanceInMiles;
     }
 }
